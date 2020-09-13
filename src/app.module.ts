285d857b8187acfe6c0824f79e21659aa610fd4e
jeremyjaouen <jjaouen@centreon.com>
2020-09-13 22:34:39 +0200

    remove useless import

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { WeekController } from './application/week/week.controller';
 import { DayController } from './application/day/day.controller';
-import { MonthController } from './application/month/month.controller';
-import { WeekModule } from './domain/week/week.module';
 import { DayModule } from './domain/day/day.module';
-import { MonthModule } from './domain/month/month.module';
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { Connection } from 'typeorm'
 import { Day as DayEntity } from './domain/day/definitions/day.entity'
@@ -15,9 +11,7 @@ import { ConfigModule } from '@nestjs/config';
 @Module(
   {
   imports: [
-    WeekModule,
     DayModule,
-    MonthModule,
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'mysql',
@@ -30,7 +24,7 @@ import { ConfigModule } from '@nestjs/config';
       synchronize: true,
     })
   ],
-  controllers: [AppController, WeekController, DayController, MonthController],
+  controllers: [AppController, DayController],
   providers: [AppService],
 })
 export class AppModule {
